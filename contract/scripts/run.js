29ff8a5012975364e947fee77da64ed74a2ded87
Atul Patil <atuluttampatil.1757@gmail.com>
2022-06-14 23:43:23 +0530

    🧪: Test the contract

--- contract/scripts/run.js
+++ contract/scripts/run.js
@@ -1,4 +1,5 @@
 
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
@@ -10,38 +11,43 @@ async function main() {
   await contract.deployed();
 
   console.log("Greeter deployed to:", contract.address);
-  const listingPrice = hre.ethers.utils.parseUnits('5',18);
+  const listingPrice = hre.ethers.utils.parseUnits('2',18);
   const royalty = hre.ethers.utils.parseUnits('1',18);
 
 
   //create tokens
-  await contract.mintToken("tokenURI",listingPrice,royalty);
-  console.log("token minte");
+  await contract.mintToken("token1",listingPrice,royalty);
+  console.log("token minted");
+  await contract.mintToken("token2",listingPrice,royalty);
+  await contract.mintToken("token3",listingPrice,royalty);
 
-  //get all the unsold items:
-//   let items = await contract.getAllMarketitems();
-//   items = await Promise.all(items.map(async i =>{
-//     const tokenURI = await contract.tokenURI(i.token)
+  const [owner,buyerAddress] = await ethers.getSigners();
+  
+  await contract.connect(buyerAddress).buyFromMarketitems(1, {value:listingPrice});
+  console.log('💰: Item bought');
+  // let ownerBalance = await hre.balanceOf(owner.address)
+  // let buyerBalance  = await hre.balanceOf(buyerAddress.address);
 
-//     let item = {
-//         price: i.price,
-//         tokenId: i.tokenId,
-//         seller: i.seller,
-//         owner: i.owner,
-//         tokenURI
-//     }
-//     return item
-//   }))
-//   console.log('items:',items)
+  // console.log('💵: Owner balance :', ownerBalance);
+  // console.log('💵: Buyer balance :', buyerBalance);
 
-  let tx1 = contract.getAllMarketitems();
-  await tx1;
 
-  tx1.then((item)=>{ 
-    console.log(item);
-    const tokenUri = await contract.tokenUri(item.tokenId);
-    console.log(tokenUri);
-  })
+ 
+  let tx1 = await contract.getAllMarketitems()
+  tx1 = await Promise.all(tx1.map(async i =>{
+    const tokenUr = await contract.tokenURI(i.tokenId);
+    let item = {
+      price: i.price.toString(),
+      tokenId: i.tokenId.toString(),
+      seller: i.seller,
+      owner:i.owner,
+      royalty:i.royalty.toString(),
+      tokenUr
+    }
+    return item
+  }))
+  console.log('Listed Items from MarketPlace',tx1);
+
 }
 
 
